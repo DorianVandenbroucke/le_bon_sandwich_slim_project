478897501914ebc2a6309982e05228ff61ee6ed5
vandenbr3u <dorian.vandenbroucke@outlook.com>
2016-11-29 14:53:42 +0000

    mise en place du router api

--- td1/api/api.php
+++ td1/api/api.php
@@ -1,3 +1,18 @@
 <?php
 
-echo "Hello world";
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require("../vendor/autoload.php");
+
+$app = new \Slim\App;
+
+$app->get(
+  "/categories/",
+  function(Request $req, Response $resp, $args){
+    $resp->getBody()->write();
+    return $resp;
+  }
+);
+
+$app->run();
